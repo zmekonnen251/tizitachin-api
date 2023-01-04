e3507302a70695b2e2d41d497c7f8c5152d15336
Zelalem <mekonnen.zelalem251@gmail.com>
2023-01-04 19:48:08 +0300

    Update cors policy

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -6,7 +6,7 @@ const credentials = (req, res, next) => {
 	// 	res.setHeader('Access-Control-Allow-Credentials', true);
 	// }
 	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Origin', '*');
+
 	res.header(
 		'Access-Control-Allow-Methods',
 		'GET,PUT,DELETE,POST,UPDATE,OPTIONS'
